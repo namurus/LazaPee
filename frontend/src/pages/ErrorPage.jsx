46bed8cda4466422e87a4ab052e994c24633463c
NgocLe-101 <ngocle266tqt@gmail.com>
2024-10-12 17:51:06 +0700

    Fixed error page not fullscreen

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -3,10 +3,18 @@ import Template from './Teamplate';
 export default function ErrorPage() {
   return (
     <Template>
-      <div className='flex flex-1 flex-col items-center justify-center'>
-        <h1 className='font-display text-6xl font-bold'>404</h1>
-        <p className='font-primary text-xl'>Page not found</p>
-      </div>
+      <ErrorContent />
     </Template>
   );
 }
+
+function ErrorContent() {
+  return (
+    <div className='flex flex-1 flex-col items-center justify-center'>
+      <h1 className='font-display text-6xl font-bold'>404</h1>
+      <p className='font-primary text-xl'>Page not found</p>
+    </div>
+  );
+}
+
+export { ErrorContent };
--- frontend/src/pages/Teamplate.jsx
+++ frontend/src/pages/Teamplate.jsx
@@ -1,20 +1,14 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import PropTypes from 'prop-types';
+import { ErrorContent } from './ErrorPage';
 
 export default function Template({ children }) {
   return (
     <div className='flex min-h-screen flex-col font-primary'>
       <Header />
-      <main className='lg:[&>*:not(.full-screen-section)]:mx-24'>
-        {children ? (
-          children
-        ) : (
-          <div className='flex flex-1 flex-col items-center justify-center'>
-            <h1 className='font-display text-6xl font-bold'>404</h1>
-            <p className='font-primary text-xl'>Page not found</p>
-          </div>
-        )}
+      <main className='flex flex-1 flex-col lg:[&>*:not(.full-screen-section)]:mx-24'>
+        {children ? children : <ErrorContent />}
       </main>
       <Footer />
     </div>
