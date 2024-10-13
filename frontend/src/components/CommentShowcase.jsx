d36fbac0506e4ee85c584287cc8953a666e62cc9
NgocLe-101 <ngocle266tqt@gmail.com>
2024-10-13 10:43:13 +0700

    add container to elements to match with layout design + fixed minor bug on comment section

---
+++ frontend/src/components/CommentShowcase.jsx
@@ -0,0 +1,104 @@
+import { useState, useEffect } from 'react';
+import SectionHeading from './SectionHeading';
+import PropTypes from 'prop-types';
+import StarRating from './StarRating';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import LoadingSpinner from './LoadingSpinner';
+
+function CommentCard({ comment }) {
+  return (
+    <div className='rounded-[1.25rem] border-2 p-6 lg:px-8 lg:py-7'>
+      <div className='mb-3'>
+        <StarRating rating={5} name={comment.id + 'comment'} />
+      </div>
+      <h3 className='mb-2 text-base font-semibold'>{comment.user.fullName}</h3>
+      <p className='text-balance text-sm font-light opacity-60'>
+        &quot;
+        {
+          'Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.'
+        }
+        &quot;
+      </p>
+    </div>
+  );
+}
+
+CommentCard.propTypes = {
+  comment: PropTypes.object.isRequired,
+};
+
+function CommentShowcase() {
+  const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentCommentIndex, setCurrentCommentIndex] = useState(0);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/comments')
+      .then((response) => response.json())
+      .then((data) => {
+        setComments(data.comments);
+        setLoading(false);
+      });
+  }, []);
+  const handleNextComment = () => {
+    setCurrentCommentIndex((prev) =>
+      prev + 1 === comments.length ? prev : prev + 1
+    );
+  };
+
+  const handlePrevComment = () => {
+    setCurrentCommentIndex((prev) => (prev - 1 < 0 ? prev : prev - 1));
+  };
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <section className='m-20'>
+      <div className='lg:container lg:mx-auto'>
+        <div className='flex gap-2'>
+          <SectionHeading
+            title='OUR HAPPY CUSTOMERS'
+            className={'my-6 flex-1 text-left'}
+          />
+          <div className='flex items-center gap-4 [&>*]:h-6 [&>*]:w-6'>
+            <button
+              onClick={() => handlePrevComment()}
+              disabled={currentCommentIndex === 0}
+            >
+              <FaArrowLeft className='w-full' />
+            </button>
+            <button
+              onClick={() => handleNextComment()}
+              disabled={currentCommentIndex === comments.length - 1}
+            >
+              <FaArrowRight className='w-full' />
+            </button>
+          </div>
+        </div>
+
+        <div className='mobile-view sm:hidden'>
+          <div className='grid grid-cols-1 gap-5 lg:grid-cols-3 lg:grid-rows-1'>
+            {comments[currentCommentIndex] && (
+              <CommentCard comment={comments[currentCommentIndex]} />
+            )}
+          </div>
+        </div>
+        <div className='desktop-view hidden sm:block'>
+          <div className='grid grid-cols-1 gap-5 lg:grid-cols-3 lg:grid-rows-1'>
+            {comments
+              .filter((_, index) => Math.abs(index - currentCommentIndex) <= 1)
+              .map((comment) => (
+                <div key={comment.id}>
+                  <CommentCard comment={comment} />
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default CommentShowcase;
