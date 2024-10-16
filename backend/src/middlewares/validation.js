cff94af8e9fa8f25c473ac0ad17a8be07f0f7605
phiho15 <117473638+phiho15@users.noreply.github.com>
2024-10-16 11:42:18 +0700

    add validation middleware

---
+++ backend/src/middlewares/validation.js
@@ -0,0 +1,14 @@
+import { validationResult } from "express-validator";
+
+export default (validations) => {
+	return async (req, res, next) => {
+		await Promise.all(validations.map((validation) => validation.run(req)));
+
+		const errors = validationResult(req);
+		if (errors.isEmpty()) {
+			return next();
+		}
+
+		return res.status(400).json({ errors: errors.array() });
+	};
+}
