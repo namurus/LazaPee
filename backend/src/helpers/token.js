1346ccf3e05b41230353f92e4cd5446d6f2cb444
phiho15 <117473638+phiho15@users.noreply.github.com>
2024-10-26 18:53:12 +0700

    fixed jwt token expires

--- backend/src/helpers/token.js
+++ backend/src/helpers/token.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 export const generateToken = (user) => { 
 	const options = {
-		expiresIn: '2 days',
+		expiresIn: '2d',
 	};
 	const token = jwt.sign(user, process.env.JWT_SECRET_KEY, options);
 	return token;
