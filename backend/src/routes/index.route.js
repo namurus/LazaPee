5f7adc0dd3beafe190b6a694bc06e1d0dbd8f85c
HanhSpring <hanhtyr@gmail.com>
2024-11-19 20:18:21 +0700

    fix bug for index.routes and category.js

--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -1,12 +1,10 @@
 import authRoutes from "./auth.route";
 import customerCategoryRoutes from './category.route'
 import adminCategoryRoutes from "./admin/category.route";
-import productRoutes from './product.route';
 import cartRoutes from './cart.route'; 
 import adminCAuthRoutes from "./admin/auth.route";
 import adminProductRoutes from "./admin/product.route";
 import productRoutes from "./product.route";
-import cartRoutes from "./cart.route";
 export default (app) => { 
 	app.use("/auth", authRoutes);
 	app.use("/products", productRoutes);
