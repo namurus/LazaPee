5b2d603b75660223b0d977fd21f5598f084ae95c
namurus <thaidinhngan@gmail.com>
2024-12-23 11:31:54 +0700

    filter color size

--- backend/src/controllers/search.controller.js
+++ backend/src/controllers/search.controller.js
@@ -2,7 +2,7 @@ import db from '@/database';
 
 export const searchProducts = async (req, res, next) => {
     try {
-        const { facet, keyword, maxPrice, minPrice, page, colors, brands } = req.query;
+        const { facet, keyword, maxPrice, minPrice, page, color, size, brands } = req.query;
 
         if (!keyword) {
             return res.status(400).json({ code: 400, message: 'Keyword is required' });
@@ -10,40 +10,54 @@ export const searchProducts = async (req, res, next) => {
 
         const whereClause = {
             [db.Sequelize.Op.and]: [
-                db.Sequelize.literal(`MATCH (productName) AGAINST (:keyword IN BOOLEAN MODE)`)
-            ]
+                db.Sequelize.literal(`MATCH (productName) AGAINST (:keyword IN BOOLEAN MODE)`),
+            ],
         };
-        // const whereClause = {
-        //     [db.Sequelize.Op.and]: [
-        //         db.Sequelize.literal(`MATCH (productName) AGAINST (:keyword IN NATURAL LANGUAGE MODE)`)
-        //     ]
-        // };
 
         const replacements = { keyword };
 
         if (facet) {
             const categories = facet.split(',');
             whereClause[db.Sequelize.Op.and].push({
-                categoryId: { [db.Sequelize.Op.in]: categories }
+                categoryId: { [db.Sequelize.Op.in]: categories },
             });
         }
 
         if (maxPrice) {
             whereClause[db.Sequelize.Op.and].push({
-                price: { [db.Sequelize.Op.lte]: maxPrice }
+                price: { [db.Sequelize.Op.lte]: maxPrice },
             });
         }
 
         if (minPrice) {
             whereClause[db.Sequelize.Op.and].push({
-                price: { [db.Sequelize.Op.gte]: minPrice }
+                price: { [db.Sequelize.Op.gte]: minPrice },
             });
         }
 
         if (brands) {
             const brandArray = brands.split(',');
             whereClause[db.Sequelize.Op.and].push({
-                brand: { [db.Sequelize.Op.in]: brandArray }
+                brand: { [db.Sequelize.Op.in]: brandArray },
+            });
+        }
+
+        // Điều kiện liên quan đến size và color
+        const skuWhereClause = {
+            [db.Sequelize.Op.or]: [],
+        };
+
+        if (size) {
+            skuWhereClause[db.Sequelize.Op.or].push({
+                attributeName: 'size',
+                value: size,
+            });
+        }
+
+        if (color) {
+            skuWhereClause[db.Sequelize.Op.or].push({
+                attributeName: 'color',
+                value: color,
             });
         }
 
@@ -53,6 +67,14 @@ export const searchProducts = async (req, res, next) => {
         const products = await db.models.Product.findAll({
             where: whereClause,
             replacements,
+            include: [
+                {
+                    model: db.models.Skus,
+                    as: 'skus',
+                    where: skuWhereClause,
+                    required: true, // Chỉ lấy các product có SKU thỏa mãn
+                },
+            ],
             limit: limit,
             offset: offset,
         });
