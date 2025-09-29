2b0d2da0067802fc451b3115f95674e7b8e22e87
namurus <thaidinhngan@gmail.com>
2025-09-29 10:06:02 +0700

    Update cors backend

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,20 @@
-export default {
-	origin: process.env.CORS_ALLOWED_ORIGIN,
-	optionsSuccessStatus: 200,
-	allowedHeaders: ['Content-Type', 'Authorization'],
+const allowedOrigins = [
+	'http://localhost:5173',
+	'https://abc.vercel.app'
+];
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (!origin || allowedOrigins.includes(origin)) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	credentials: true,
+	allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
 	exposedHeaders: ['Content-Length', 'Content-Type', 'token'],
+	optionsSuccessStatus: 200,
 };
+
+app.use(cors(corsOptions));
